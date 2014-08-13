e744f698ef10e917535395f8a81d1843170ba017
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2014-08-13 03:19:22 +0200

    Use instrumented tokenizer.
    Add timestamps to tokenizer wrapper.

--- EHU-tok/run.sh
+++ EHU-tok/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rootDir=/home/newsreader/components/EHU-tok
-java -jar ${rootDir}/ixa-pipe-tok-1.5.0.jar tok -l en --inputkaf
+rootDir=/home/newsreader/components_source/ixa-pipe-tok/target
+date +'EHU-tok start command: %s%3N' 1>&2
+java -jar ${rootDir}/ixa-pipe-tok-1.5.1-timed.jar tok -l en --inputkaf
+date +'EHU-tok end command: %s%3N' 1>&2
+
 
