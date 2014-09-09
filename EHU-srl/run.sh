2526aafbd2012e0287b2328ceba96bc5a82d0910
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2014-09-09 17:08:29 +0200

    Failed to see end command timestamp due to exec.

--- EHU-srl/run.sh
+++ EHU-srl/run.sh
@@ -2,5 +2,5 @@
 
 rootDir=/home/newsreader/components/EHU-srl
 date +'Timestamp EHU-srl start command: %s%3N' 1>&2
-exec java -jar -Xms2500m ${rootDir}/IXA-EHU-srl-1.0-timed.jar en
+java -jar -Xms2500m ${rootDir}/IXA-EHU-srl-1.0-timed.jar en
 date +'Timestamp EHU-srl end command: %s%3N' 1>&2
