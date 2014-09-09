5c520d4c3d503d486e4c4cf112b23b3aeed24eba
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2014-09-10 01:52:15 +0200

    Measure wall clock in run.sh + use instrumented jar file

--- EHU-nerc/run.sh
+++ EHU-nerc/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 rootDir=/home/newsreader/components/EHU-nerc
-java -jar ${rootDir}/ixa.pipe.nerc-1.0.0.jar tag -f baseline --model /en/en-nerc-perceptron-baseline-c0-b3-ontonotes-4.0.bin
+date +'Timestamp EHU-nerc start command: %s%3N' 1>&2
+java -jar ${rootDir}/ixa.pipe.nerc-1.0.0-timed.jar tag -f baseline --model /en/en-nerc-perceptron-baseline-c0-b3-ontonotes-4.0.bin
+date +'Timestamp EHU-nerc end command: %s%3N' 1>&2
