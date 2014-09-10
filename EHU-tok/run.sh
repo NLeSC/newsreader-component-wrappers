743ad1b19e1982a9f59c4efa6903e70e10306043
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2014-09-10 06:34:48 +0200

    Use copied version of timed jar.

--- EHU-tok/run.sh
+++ EHU-tok/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rootDir=/home/newsreader/components_source/ixa-pipe-tok/target
+rootDir=/home/newsreader/components/ixa-pipe-tok
 date +'Timestamp EHU-tok start command: %s%3N' 1>&2
 java -jar ${rootDir}/ixa-pipe-tok-1.5.1-timed.jar tok -l en --inputkaf
 date +'Timestamp EHU-tok end command: %s%3N' 1>&2
