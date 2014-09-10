c11b0ef99a50e31e938a9b91c93ce51c0f5dd876
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2014-09-10 04:08:50 +0200

    Add timings around VUA-multiwordtagger.

--- VUA-multiwordtagger/run.sh
+++ VUA-multiwordtagger/run.sh
@@ -1,3 +1,5 @@
 #### Multiwordtagger using wordnet-lmf as a resource
 cd /home/newsreader/components/VUA-multiwordtagger
+date +'Timestamp VUA-mw start command: %s%3N' 1>&2
 java -Xmx812m -cp "./lib/KafMultiWordTagger-0.0.1-jar-with-dependencies.jar" eu.kyotoproject.multiwordtagger.MultiTaggerSingleNaf --conf-file "./conf/mwtagger.cfg"
+date +'Timestamp VUA-mw end command: %s%3N' 1>&2
