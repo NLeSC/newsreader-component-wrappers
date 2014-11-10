eb044be6b504578012f645469487ee9488111b69
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2014-11-10 11:50:03 +0100

    Add timings to VUA-eventcoref, VUA-factuality, VUA-opinion-miner

--- VUA-factuality/run.sh
+++ VUA-factuality/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+date +'Timestamp VUA-factuality start command: %s%3N' 1>&2
+
 # use non standard location for perl libraries
 export PERL5LIB=$PERL5LIB:/home/newsreader/opt/lib/perl5
 rootDir=/home/newsreader/components/VUA-factuality
@@ -8,17 +10,27 @@ cd ${rootDir}
 # Create a begin timestamp so we can log how long it takes to process a file
 perl beginTimestamp.pl
 
+date +'Timestamp VUA-factuality start in-command: %s%3N' 1>&2
+
 # Convert NAF file to Mallet input file 
 perl NAFToMalletInputFactuality.pl > malletinput.tab
+
+date +'Timestamp VUA-factuality start work: %s%3N' 1>&2
 
 # Run the mallet classifier 
 mallet-2.0.7/bin/csv2classify --input malletinput.tab --output malletoutput.txt --classifier MyMaxEntFactuality.classifier
 
+date +'Timestamp VUA-factuality end work: %s%3N' 1>&2
+
 # Sort the output in order to be able to select the best prediction and its confidence
 perl sortMalletOutput.pl malletoutput.txt > malletoutput.sorted
+
+date +'Timestamp VUA-factuality end in-command: %s%3N' 1>&2
 
 # Write back to NAF 
 perl convertMalletToNAF.pl temp.naf malletoutput.sorted
 
 # Clean up
 rm temp.naf malletinput.tab malletoutput.txt malletoutput.sorted begintimestamp.txt
+
+date +'Timestamp VUA-factuality start command: %s%3N' 1>&2
--- VUA-opinion-miner/run.sh
+++ VUA-opinion-miner/run.sh
@@ -4,4 +4,6 @@ export PYTHONPATH=$PYTHONPATH:/home/newsreader/opt/lib/python2.6/
 rootDir=/home/newsreader/components/VUA-opinion-miner/opinion_miner_deluxe
 
 cd ${rootDir}
+date +'Timestamp VUA-opinion-miner start command: %s%3N' 1>&2
 python ${rootDir}/classify_kaf_file.py ${rootDir}/my_train.cfg #2> /dev/null
+date +'Timestamp VUA-opinion-miner end command: %s%3N' 1>&2
